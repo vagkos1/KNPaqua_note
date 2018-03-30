7b504ebdd407773235b8d718e60b2e344c00b7d5
Vagelis Kostopoulos <kostopoulos.e@gmail.com>
2018-03-30 13:16:37 +0100

    Service / Use Twig

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -5,15 +5,29 @@ namespace AppBundle\Controller;
 
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
-class GenusController
+/**
+ * Class GenusController
+ * By extending Symfony's Controller, we gain access to the container since that Controller uses the ContainerAwareTrait
+ *
+ * @package AppBundle\Controller
+ */
+class GenusController extends Controller
 {
     /**
      * @Route("/genus/{genusName}")
      */
     public function showAction(string $genusName) : Response
     {
-        return new Response('The genus: '.$genusName);
+        // for some reason, the templating service is not currently available in the container so we'll use the twig one.
+        $templating = $this->container->get('twig');
+
+        $html = $templating->render('genus/show.html.twig', [
+            'name' => $genusName
+        ]);
+
+        return new Response($html);
     }
 }
