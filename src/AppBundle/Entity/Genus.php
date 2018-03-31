6d951cec6373872df1ddf8d8d6a4e4a524557a23
Vagelis Kostopoulos <kostopoulos.e@gmail.com>
2018-03-31 04:40:01 +0100

    Doctrine / Add a Genus Entity

---
+++ src/AppBundle/Entity/Genus.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="genus")
+ */
+class Genus
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     * will map to a varchar() in mySQL
+     */
+    private $name;
+}
