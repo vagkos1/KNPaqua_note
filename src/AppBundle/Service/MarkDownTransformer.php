f809fe685c7fa74ef9fb51a207dd418649838265
Vagelis Kostopoulos <kostopoulos.e@gmail.com>
2018-04-02 04:49:43 +0100

    Service / Inject cache into MarkdownTransformer

--- src/AppBundle/Service/MarkDownTransformer.php
+++ src/AppBundle/Service/MarkDownTransformer.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Service;
 
 
+use Doctrine\Common\Cache\Cache;
 use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 use Knp\Bundle\MarkdownBundle\Parser\MarkdownParser;
 
@@ -11,14 +12,27 @@ class MarkDownTransformer
     /** @var MarkdownParser */
     private $markdownParser;
 
-    public function __construct(MarkdownParserInterface $markdownParser)
+    /** @var Cache  */
+    private $cache;
+
+    public function __construct(MarkdownParserInterface $markdownParser, Cache $cache)
     {
         $this->markdownParser = $markdownParser;
+        $this->cache = $cache;
     }
 
     public function parse(string $str) : string
     {
-        return $this->markdownParser
+        $key = md5($str);
+        if ($this->cache->contains($key)) {
+            return $this->cache->fetch($key);
+        }
+
+        sleep(3);
+        $str = $this->markdownParser
             ->transformMarkdown($str);
+        $this->cache->save($key, $str);
+
+        return $str;
     }
 }
