6fbc5fcaf9e78152ea0f509f26e52b5981c198d4
Vagelis Kostopoulos <kostopoulos.e@gmail.com>
2018-04-02 08:34:21 +0100

    Form / Build and render GenusFormType to add genuses

--- src/AppBundle/Controller/Admin/GenusAdminController.php
+++ src/AppBundle/Controller/Admin/GenusAdminController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller\Admin;
 
+use AppBundle\Form\GenusFormType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -29,6 +30,11 @@ class GenusAdminController extends Controller
      */
     public function newAction()
     {
-        // let's go to work!
+        // uses the form.factory service in the background
+        $form = $this->createForm(GenusFormType::class);
+
+        return $this->render('admin/genus/new.html.twig', [
+            'genusForm' => $form->createView()
+        ]);
     }
 }
