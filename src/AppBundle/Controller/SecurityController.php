d03b870172b62d63ace71d0e80821259f4be4e24
Vagelis Kostopoulos <kostopoulos.e@gmail.com>
2018-04-02 23:29:53 +0100

    Authentication / Render the login form
    We'll deal with the Form processing later (separately).
    
    The reason we create a \AppBundle\Form\LoginForm instead of creating a form in twig, as is customary for login forms (https://symfony.com/doc/current/security/form_login_setup.html), is to take advantage of the default usage of bootstrap for normal forms.
    
    This is a rare case where we are not binding the loginForm with a class.

---
+++ src/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace AppBundle\Controller;
+
+
+use AppBundle\Form\LoginForm;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SecurityController extends Controller
+{
+    /**
+     * @Route("/login", name="security_login")
+     */
+    public function loginAction()
+    {
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        // if the user failed login, we prepopulate the username
+        $form = $this->createForm(LoginForm::class, [
+            '_username' => $lastUsername,
+        ]);
+
+        return $this->render('security/login.html.twig', array(
+            'form' => $form->createView(),
+            'error'         => $error,
+        ));
+    }
+}
