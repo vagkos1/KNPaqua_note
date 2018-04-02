8627fc3aa7f8ff9ec3f52d63936c4de68948b087
Vagelis Kostopoulos <kostopoulos.e@gmail.com>
2018-04-02 08:41:46 +0100

    Form / Process on POST

--- src/AppBundle/Controller/Admin/GenusAdminController.php
+++ src/AppBundle/Controller/Admin/GenusAdminController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller\Admin;
 use AppBundle\Form\GenusFormType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @Route("/admin")
@@ -28,10 +29,19 @@ class GenusAdminController extends Controller
     /**
      * @Route("/genus/new", name="admin_genus_new")
      */
-    public function newAction()
+    public function newAction(Request $request)
     {
         // uses the form.factory service in the background
         $form = $this->createForm(GenusFormType::class);
+
+        // only handles data on POST.
+        // If the user just navigates to the form (GET), the handleRequest does nothing.
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            dump($form->getData());
+            die();
+        }
 
         return $this->render('admin/genus/new.html.twig', [
             'genusForm' => $form->createView()
