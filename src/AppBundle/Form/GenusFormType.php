2b2d58bc30336d68cb33a718e368ee8b676fdd47
Vagelis Kostopoulos <kostopoulos.e@gmail.com>
2018-04-02 16:36:07 +0100

    Form / Use different Field Types & options

--- src/AppBundle/Form/GenusFormType.php
+++ src/AppBundle/Form/GenusFormType.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -10,14 +11,24 @@ class GenusFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        // https://symfony.com/doc/current/reference/forms/types.html
         $builder
             ->add('name')
-            // genus.subFamily is a M-1 to subFamily, so the builder will try to add a select box.
+            // genus.subFamily is a M-1 to subFamily, so the builder will try to add a select dropdown.
             // We'll need a __toString() in subFamily for the values of that dropdown
-            ->add('subFamily')
+            // By default Symfony uses a Form EntityType field for this and fetches the options by querying the Entity
+            // by using null as the second argument, Symfony will continue to use EntityType field and guess the class
+            ->add('subFamily', null, [
+                'placeholder' => 'Choose a Sub Family'
+            ])
             ->add('speciesCount')
             ->add('funFact')
-            ->add('isPublished')
+            ->add('isPublished', ChoiceType::class, [
+                'choices' => [
+                    'Yes' => true,
+                    'No' => false
+                ]
+            ])
             ->add('firstDiscoveredAt')
         ;
     }
