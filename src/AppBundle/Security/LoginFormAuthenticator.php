614c47788b37c0cbcaaedf392952b6b99e853d20
Vagelis Kostopoulos <kostopoulos.e@gmail.com>
2018-04-03 01:55:48 +0100

    Authentication : Use the password encoder whenever the user logs in

--- src/AppBundle/Security/LoginFormAuthenticator.php
+++ src/AppBundle/Security/LoginFormAuthenticator.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -25,15 +26,18 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
     private $formFactory;
     private $em;
     private $router;
+    private $passwordEncoder;
 
     public function __construct(
         FormFactoryInterface $formFactory,
         EntityManagerInterface $em,
-        RouterInterface $router
+        RouterInterface $router,
+        UserPasswordEncoderInterface $passwordEncoder
     ) {
         $this->formFactory = $formFactory;
         $this->em = $em;
         $this->router = $router;
+        $this->passwordEncoder = $passwordEncoder;
     }
 
     /**
@@ -78,7 +82,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
     {
         $password = $credentials['_password'];
 
-        if ($password == 'iliketurtles') {
+        if ($this->passwordEncoder->isPasswordValid($user, $password)) {
             return true;
         }
 
