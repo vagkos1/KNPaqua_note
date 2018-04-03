7f0257d0d90eab015d2c84e260c16c9dbd41c215
Vagelis Kostopoulos <kostopoulos.e@gmail.com>
2018-04-03 01:26:43 +0100

    Authentication / Add a password to the User Class

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -26,6 +26,16 @@ class User implements UserInterface
      */
     private $email;
 
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $password;
+
+    // we'll set the plain password on the user and encode it automatically via a Doctrine listener when it saves.
+    // we are not going to persist this with Doctrine - never store plain text passwords
+    // this is just a temporary storage place during a request
+    private $plainPassword;
+
     public function getRoles()
     {
         return ['ROLE_USER'];
@@ -33,8 +43,10 @@ class User implements UserInterface
 
     public function getPassword()
     {
+        return $this->password;
     }
 
+    // not necessary since we'll use bcrypt, which comes with a built in mechanism to salt passwords.
     public function getSalt()
     {
     }
@@ -44,12 +56,36 @@ class User implements UserInterface
         return $this->email;
     }
 
+    // Symfony calls this after logging in.
+    //// minor security measure to prevent the plain password from accidentally getting saved.
     public function eraseCredentials()
     {
+        $this->plainPassword = null;
     }
 
     public function setEmail(string $email): void
     {
         $this->email = $email;
     }
+
+    public function setPassword($password): void
+    {
+        $this->password = $password;
+    }
+
+    public function getPlainPassword()
+    {
+        return $this->plainPassword;
+    }
+
+    public function setPlainPassword($plainPassword): void
+    {
+        $this->plainPassword = $plainPassword;
+
+        // guarantees that the User entity looks "dirty" to Doctrine
+        // when changing the plainPassword (since we are not saving the plain password in the DB)
+        $this->password = null;
+    }
+
+
 }
