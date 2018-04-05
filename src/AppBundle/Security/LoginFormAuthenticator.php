a09b1950ef6e90be584e0865780c71b5845dd50d
Vagelis Kostopoulos <kostopoulos.e@gmail.com>
2018-04-05 03:18:13 +0100

    Authentication / Guard - Finish the Authenticator and hook it up
    Register + Autowire LoginFormAuthenticator.
    Activate the Guard Authenticator in the firewalls section.

--- src/AppBundle/Security/LoginFormAuthenticator.php
+++ src/AppBundle/Security/LoginFormAuthenticator.php
@@ -3,24 +3,45 @@
 namespace AppBundle\Security;
 
 
+use AppBundle\Entity\User;
 use AppBundle\Form\LoginForm;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
 class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
 {
-    private $formFactory;
+    use TargetPathTrait;
 
-    public function __construct(FormFactoryInterface $formFactory)
-    {
+    private $formFactory;
+    private $em;
+    private $router;
+
+    public function __construct(
+        FormFactoryInterface $formFactory,
+        EntityManagerInterface $em,
+        RouterInterface $router
+    ) {
         $this->formFactory = $formFactory;
+        $this->em = $em;
+        $this->router = $router;
     }
 
-    // is the request a login form submit? If not, the authenticator doesn't have to do anything
+    /**
+     * is the request a login form submit? If not, the authenticator doesn't have to do anything
+     * Since LoginForm::class is not tied to a class (no binding), then $data is just an associative array
+     *
+     * @param Request $request
+     * @return array|null
+     */
     public function getCredentials(Request $request)
     {
         $isLoginSubmit = $request->getPathInfo() == '/login' && $request->isMethod('POST');
@@ -35,17 +56,44 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
         return $data;
     }
 
-    public function getUser($credentials, UserProviderInterface $userProvider)
+    // Called if $this->getCredentials returns something other than null
+    public function getUser($credentials, UserProviderInterface $userProvider) : User
     {
-        // TODO: Implement getUser() method.
+        $username = $credentials['_username'];
+
+        return $this->em->getRepository('AppBundle:User')
+            ->findOneBy(['email' => $username]);
     }
 
-    public function checkCredentials($credentials, UserInterface $user)
+    // called if $this->getUser returns a User
+    public function checkCredentials($credentials, UserInterface $user) : bool
     {
+        $password = $credentials['_password'];
+
+        if ($password == 'iliketurtles') {
+            return true;
+        }
+
+        return false;
     }
 
+    // in case the credentials failed, user gets redirected via this method
     protected function getLoginUrl()
     {
-        // TODO: Implement getLoginUrl() method.
+        return $this->router->generate('security_login');
+    }
+
+    // in case the user succeeds, they get redirected via this method
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
+    {
+        // if the user hits a secure page and start() was called, this was
+        // the URL they were on, and probably where you want to redirect to
+        $targetPath = $this->getTargetPath($request->getSession(), $providerKey);
+
+        if (!$targetPath) {
+            $targetPath = $this->router->generate('homepage');
+        }
+
+        return new RedirectResponse($targetPath);
     }
 }
