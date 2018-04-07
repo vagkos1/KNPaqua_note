00a8155561d378ec42bc975366a67c242830bdc4
Vagelis Kostopoulos <kostopoulos.e@gmail.com>
2018-04-07 21:54:29 +0100

    Authorization / Use Role hierarchy to group permissions for a role
    General Authorization Strategy:
    1. lock down different sections using role names that describe what it's protecting.
    2. in security.yml, create group based roles and assign each the permissions they should have.

--- src/AppBundle/Controller/Admin/GenusAdminController.php
+++ src/AppBundle/Controller/Admin/GenusAdminController.php
@@ -10,7 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
- * @Security("is_granted('ROLE_ADMIN')")
+ * @Security("is_granted('ROLE_MANAGE_GENUS')")
  * @Route("/admin")
  */
 class GenusAdminController extends Controller
