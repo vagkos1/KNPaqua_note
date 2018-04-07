bf4cbc489ce9559d18fc1a270a8394f8d1f4368d
Vagelis Kostopoulos <kostopoulos.e@gmail.com>
2018-04-07 20:17:06 +0100

    Authorization / Deny access for /admin
    roles must start with ROLE_
    
    There are two main ways to deny access:
    
    - access_control (in security.yml - it's the simplest)
    Symfony looks for a matching access control from top to bottom and stops as soon as it finds the first match.
    (we can lock every page with an access_control and then whitelist the few publicpages with an access_control entry above that)
    (we can also force the user to visit a specific part of the website via https)
    
    - inside the controllers, using he authorization_checker service
    a) We can use "createAccessDeniedException" if the user does not have the role we specify.
    b) We can use "denyAccessUnlessGranted('ROLE_blabla')
    c) We can use annotations (@Security("is_granted('ROLE_ADMIN')") either above a controller or above the whole class.

--- src/AppBundle/Controller/Admin/GenusAdminController.php
+++ src/AppBundle/Controller/Admin/GenusAdminController.php
@@ -5,10 +5,12 @@ namespace AppBundle\Controller\Admin;
 use AppBundle\Entity\Genus;
 use AppBundle\Form\GenusFormType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
+ * @Security("is_granted('ROLE_ADMIN')")
  * @Route("/admin")
  */
 class GenusAdminController extends Controller
