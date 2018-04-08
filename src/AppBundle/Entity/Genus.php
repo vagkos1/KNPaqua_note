0e2f845166a03ac951394084b12377e2175efeec
Vagelis Kostopoulos <kostopoulos.e@gmail.com>
2018-04-08 19:10:47 +0100

    Doctrine / Join across M-N + EXTRA_LAZY Fetch

--- src/AppBundle/Entity/Genus.php
+++ src/AppBundle/Entity/Genus.php
@@ -75,7 +75,7 @@ class Genus
     private $slug;
 
     /**
-     * @ORM\ManyToMany(targetEntity="AppBundle\Entity\User", inversedBy="studiedGenuses")
+     * @ORM\ManyToMany(targetEntity="AppBundle\Entity\User", inversedBy="studiedGenuses", fetch="EXTRA_LAZY")
      * @ORM\JoinTable(name="genus_scientist")
      */
     private $genusScientists;
--- src/AppBundle/Repository/GenusRepository.php
+++ src/AppBundle/Repository/GenusRepository.php
@@ -19,6 +19,9 @@ class GenusRepository extends EntityRepository
             // using the inverse side mapping of the relationship
             ->leftJoin('genus.notes', 'genus_note')
             ->orderBy('genus_note.createdAt', 'DESC')
+            // join on a relation property
+            ->leftJoin('genus.genusScientists', 'genusScientist')
+            ->addSelect('genusScientist')
             ->getQuery()
             ->execute();
         // use execute() to get an array of results
