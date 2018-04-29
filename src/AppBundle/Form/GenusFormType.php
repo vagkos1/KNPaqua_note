6dfa10ef12ed5e26d3f92c40cdea37f2e255dd43
vagkos <kostopoulos.e@gmail.com>
2018-04-30 00:03:41 +0100

    Form / Embedded subForms using CollectionType

--- src/AppBundle/Form/GenusFormType.php
+++ src/AppBundle/Form/GenusFormType.php
@@ -3,12 +3,11 @@
 namespace AppBundle\Form;
 
 use AppBundle\Entity\SubFamily;
-use AppBundle\Entity\User;
 use AppBundle\Repository\SubFamilyRepository;
-use AppBundle\Repository\UserRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -46,14 +45,8 @@ class GenusFormType extends AbstractType
                 ],
                 'html5' => false,
             ])
-            ->add('genusScientists', EntityType::class, [
-                'class' => User::class,
-                'multiple' => true,
-                'expanded' => true,
-                'choice_label' => 'email',
-                'query_builder' => function (UserRepository $repo) {
-                    return $repo->createIsScientistQueryBuilder();
-                }
+            ->add('genusScientists', CollectionType::class, [
+                'entry_type' => GenusScientistEmbeddedForm::class
             ])
         ;
     }
